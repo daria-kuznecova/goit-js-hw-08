b035f10fe5a06f585957d31a4e9f2edf235eee96
daria-kuznecova <kovalenko18101990@gmail.com>
2023-05-06 20:41:53 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(item => {
+  const galleryItem = document.createElement('div');
+  galleryItem.className = 'gallery__item';
+  const galleryLink = document.createElement('a');
+  galleryLink.className = 'gallery__link';
+  galleryLink.href = item.original;
+  const galleryImage = document.createElement('img');
+  galleryImage.className = 'gallery__image';
+  galleryImage.src = item.preview;
+  galleryImage.setAttribute('data-source', item.original);
+  galleryImage.alt = item.description;
+
+  galleryItem.append(galleryLink);
+  galleryItem.append(galleryImage);
+  items.push(galleryItem);
+});
+
+list.append(...items);
+
+list.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName != 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: () => document.addEventListener('keydown', onEscPress),
+      onClose: () => document.removeEventListener('keydown', onEscPress),
+    }
+  );
+  instance.show();
+
+  function onEscPress(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+});
