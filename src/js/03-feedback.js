c4d3117fb87e6a78fd386d5abb55af5d1c6612bc
daria-kuznecova <kovalenko18101990@gmail.com>
2023-05-07 23:21:49 +0300

    task 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('#feedback-form');
+const inputEmail = document.querySelector('.input');
+const textAreaMessage = document.querySelector('.textarea');
+const FEEDBACK_FORM_KEY = 'feedback-form-state';
+
+// updateInput();
+// form.addEventListener('input', saveInformation);
+
+function saveInformation(event) {
+  localStorage.setItem(
+    FEEDBACK_FORM_KEY,
+    JSON.stringify({
+      email: inputEmail.ariaValueMax,
+      message: textAreaMessage.value,
+    })
+  );
+}
