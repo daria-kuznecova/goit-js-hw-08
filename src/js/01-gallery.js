4711aea290de4e48115a04ea1cc4782c60e331b6
daria-kuznecova <kovalenko18101990@gmail.com>
2023-05-07 15:23:43 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,46 +6,27 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 const list = document.querySelector('.gallery');
-const items = [];
 
-galleryItems.forEach(item => {
-  const galleryItem = document.createElement('div');
-  galleryItem.className = 'gallery__item';
-  const galleryLink = document.createElement('a');
-  galleryLink.className = 'gallery__link';
-  galleryLink.href = item.original;
-  const galleryImage = document.createElement('img');
-  galleryImage.className = 'gallery__image';
-  galleryImage.src = item.preview;
-  galleryImage.setAttribute('data-source', item.original);
-  galleryImage.alt = item.description;
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join('');
 
-  galleryItem.append(galleryLink);
-  galleryItem.append(galleryImage);
-  items.push(galleryItem);
+list.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: '250',
 });
 
-list.append(...items);
-
-list.addEventListener('click', event => {
-  event.preventDefault();
-  if (event.target.nodeName != 'IMG') {
-    return;
-  }
-
-  const instance = basicLightbox.create(
-    `
-    <img src="${event.target.dataset.source}" width="800" height="600">`,
-    {
-      onShow: () => document.addEventListener('keydown', onEscPress),
-      onClose: () => document.removeEventListener('keydown', onEscPress),
-    }
-  );
-  instance.show();
-
-  function onEscPress(event) {
-    if (event.code === 'Escape') {
-      instance.close();
-    }
-  }
-});
+console.log(galleryItems);
