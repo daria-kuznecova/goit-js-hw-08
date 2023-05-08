0254ca749f4be83d7b23e5f257a011e754c2a425
daria-kuznecova <kovalenko18101990@gmail.com>
2023-05-08 16:45:42 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,47 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('#feedback-form');
-const inputEmail = document.querySelector('.input');
-const textAreaMessage = document.querySelector('.textarea');
-const FEEDBACK_FORM_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textArea = document.querySelector('textarea');
+// const button = document.querySelector('button');
+const STORAGE_KEY = 'feedback-form-state';
+// const formData = {
+//   email: '',
+//   message: '',
+// };
 
-// updateInput();
-// form.addEventListener('input', saveInformation);
+// updateDate();
 
-function saveInformation(event) {
+// button.addEventListener('submit', saveDate);
+
+function formEl() {
   localStorage.setItem(
-    FEEDBACK_FORM_KEY,
-    JSON.stringify({
-      email: inputEmail.ariaValueMax,
-      message: textAreaMessage.value,
-    })
+    STORAGE_KEY,
+    JSON.stringify({ email: input.value, message: textArea.value })
   );
 }
+
+function getFeedBack() {
+  const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!data) return;
+  input.value = data.email;
+  textArea.value = data.message;
+}
+
+function submit(event) {
+  event.preventDefault();
+  if (input.value.trim() != '' && textArea.value.trim() != '') {
+    console.log({
+      email: input.value,
+      message: textArea.value,
+    });
+  }
+  form.reset();
+  localStorage.clear();
+}
+
+input.addEventListener('input', throttle(formEl, 500));
+textArea.addEventListener('input', throttle(formEl, 500));
+form.addEventListener('submit', submit);
+
+getFeedBack();
